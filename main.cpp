900f5e47705dc0012baf93603a40ed6d8addf57f
carryinfo <carryinfo@163.com>
2014-10-19 09:56:36 +0800

    add stagefright message mechanism

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <log/log.h>
+#include <media/stagefright/foundation/AHandler.h>
+#include <media/stagefright/foundation/AMessage.h>
+#include <media/stagefright/foundation/ALooper.h>
+
 #define LOG_TAG "MAIN"
+
+using namespace android;
+
+#define		RECEIVE_ID	10
+
+enum {
+	kReceiveID,
+};
+
+class Receiver:public AHandler{
+protected:
+    virtual void onMessageReceived(const sp<AMessage> &msg){
+    	switch(msg->what()){
+    	case kReceiveID:
+    		ALOGD("get the kReceiveID msg");
+    		break;
+    	default:break;
+    	}
+    }
+};
+
 int main(){
 
-	ALOGD("hello world!");
+	sp<Receiver> receiver(new Receiver);
+	sp<ALooper> looper(new ALooper);
+	looper->registerHandler(receiver);
+	looper->start();
+
+	sp<AMessage> msg(new AMessage(kReceiveID,receiver->id()));
+	msg->post();
+
+	sleep(3);
+
 	return 0;
 }
